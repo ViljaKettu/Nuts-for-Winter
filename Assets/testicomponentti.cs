b480c113dd57f987c7d57bbc2b81d45f0b6b0568
ahkiaaso <35724769+ahkiaaso@users.noreply.github.com>
2018-02-20 13:40:55 +0200

    viimeset muutokset ja pari assettia

---
+++ Assets/testicomponentti.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testicomponentti : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("OnTriggerEnter");
+	//	animation.enabled = true;
+		//TrapStartAnimation.GetComponent<Animator> ().Play ("BearTrap");
+
+	}
+}
