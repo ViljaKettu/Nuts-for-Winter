3247ed89b00d470062813284423b4903348a4d05
Silagul <35958322+Silagul@users.noreply.github.com>
2018-02-20 14:15:24 +0200

    Hahmon ja kerättävien kuvia

---
+++ Assets/Scripts/Pickup_Rotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup_Rotate : MonoBehaviour {
+    
+	void Update ()
+    {
+        //transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime); //<-- Kerättävät pähkinät pyörivät (hyvä vai huono?)
+
+	}
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // Rotate the object around its local X axis at 1 degree per second
+        // transform.Rotate(Vector3.right * Time.deltaTime);
+
+        // ...also rotate around the World's Y axis
+        //transform.Rotate(Vector3.up * Time.deltaTime, Space.World);
+        //transform.Rotate(new Vector3(0,0,1) * Time.deltaTime, Space.World);
+    }
+}
