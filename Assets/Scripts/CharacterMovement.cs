7212153842aa246ab96a70d4a0cb6f4d4f14f374
Vilja <viljakettu@gmail.com>
2018-02-21 09:50:44 +0200

    katsotaan mitä tapathuu

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float speed;
+    public Text ScoreText;
+    public Text WinText;
+
+    private Rigidbody2D rb2d;
+    private int count;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        SetScoreText();
+        WinText.text = ""; //Valinnainen
+        //Hahmo ei ala pyöriä törmätessään asioihin.
+        rb2d.freezeRotation = true;
+    }
+
+    void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        if (moveHorizontal == 0)
+        {
+        }
+        else if (moveHorizontal < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 90);
+        }
+        else if (moveHorizontal > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, -90);
+        }
+
+        if (moveVertical == 0)
+        {
+        }
+        else if (moveVertical < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, -180);
+        }
+        else if (moveVertical > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb2d.velocity = movement * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetScoreText();
+        }
+    }
+
+    void SetScoreText()
+    {
+        ScoreText.text = "Count: " + count.ToString();
+
+        if (count >= 7) //Valinnainen
+        {
+            WinText.text = "You win!";
+        }
+    }
+}
---
+++ Assets/Scripts/ScoreCount1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreCount1 : MonoBehaviour
+{
+    public int count;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //count = 0;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        
+    }
+
+    //void OnTriggerEnter2D(Collider2D other)
+    //{
+        //if (other.gameObject.CompareTag("Pickup"))
+        //{
+            //other.gameObject.SetActive(false);
+            //count = count + 1;
+        //}
+    //}
+}
